f7562cbd3323d81fb27d21db3a62df62c077ec46
Yun Gu <guy36@cardiff.ac.uk>
2021-11-03 22:14:19 +0000

    card

---
+++ src/components/Card.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id = "card">
+        <img src="#" alt="">
+        <p class = "title">title</p>
+        <button>view details</button> 
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Card",
+    }
+</script>
+
+<style scoped>
+#card {
+    padding: 2em;
+    margin: 2em;
+    height: 30vh;
+    min-width: 200px;
+    max-width: 30vw;
+    border-radius: 10px;
+    box-shadow: 0 0 2em #f0f0f0 ;
+    background-color: #fff;
+}
+
+.title {
+    color: rgb(0, 0, 0);
+}
+</style>
